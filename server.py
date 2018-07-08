570ea58f81031af1a13add9f56b16e53d8f7b88e
ravikkanani <40619580+ravikkanani@users.noreply.github.com>
2018-07-08 22:11:40 +0530

    Add files via upload

--- server.py
+++ server.py
@@ -7,32 +7,12 @@ from cricbuzz import Cricbuzz
 from flask import Response
 app = Flask(__name__)
 
-@app.route('/getMatches')
+@app.route('/12')
 def getMatches():
     c = Cricbuzz()
     list =  c.matches()
     return Response(json.dumps(list),  mimetype='application/json')
 
-@app.route('/livescore')
-def getMatchInfo():
-    matchId = request.args.get('matchId')
-    c = Cricbuzz()
-    list =  c.livescore(matchId)
-    return Response(json.dumps(list),  mimetype='application/json')
-
-@app.route('/scorecard')
-def scorecard():
-    matchId = request.args.get('matchId')
-    c = Cricbuzz()
-    list = c.scorecard(matchId)
-    return Response(json.dumps(list),  mimetype='application/json')
-
-@app.route('/commentary')
-def commentary():
-    matchId = request.args.get('matchId')
-    c = Cricbuzz()
-    list = c.commentary(matchId)
-    return Response(json.dumps(list),  mimetype='application/json')
 
 if __name__ == '__main__':
     app.run(
